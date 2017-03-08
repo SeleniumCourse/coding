755fe8917dabd46a628e0bd7d08940afd20ca077
JoaquinGonzales <joaquinjqn2@gmail.com>
2017-03-08 19:43:56 -0400

    first Commit

---
+++ src/main/java/com/jalasoft/selenium/joaquin/shape/Shape.java
@@ -0,0 +1,9 @@
+package com.jalasoft.selenium.joaquin.shape;
+
+/**
+ * Created by Joaquin Gonzales on 3/6/2017.
+ */
+public abstract class Shape {
+    public abstract double calculateArea();
+    public abstract double calculateperimetro();
+}
