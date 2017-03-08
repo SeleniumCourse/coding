7eafed7441f871e29df0751463bd5acfaf93f6fe
Sl3v7n <slevin.v7@gmail.com>
2017-03-08 19:44:32 -0400

    Updating

---
+++ src/main/java/com/jalasoft/selenium/alejandro/Circle.java
@@ -0,0 +1,21 @@
+package com.jalasoft.selenium.alejandro;
+
+/**
+ * Created by Alejandro Alcocer on 3/6/2017.
+ */
+public class Circle extends Shape{
+    public double radius;
+    public  Circle(double radius){
+        this.radius=radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return 2*Math.PI*Math.pow(radius, 2);
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2*Math.PI*radius;
+    }
+}
