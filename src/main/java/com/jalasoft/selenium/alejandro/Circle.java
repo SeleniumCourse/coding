a09fcba30d78bb819c28aa1df8c4bc1a173327a5
Sl3v7n <slevin.v7@gmail.com>
2017-03-13 14:32:29 -0400

    test

--- src/main/java/com/jalasoft/selenium/alejandro/Circle.java
+++ src/main/java/com/jalasoft/selenium/alejandro/Circle.java
@@ -3,7 +3,7 @@ package com.jalasoft.selenium.alejandro;
 /**
  * Created by Alejandro Alcocer on 3/6/2017.
  */
-public class Circle extends Shape{
+public class Circle extends Shape {
     public double radius;
     public  Circle(double radius){
         this.radius=radius;
