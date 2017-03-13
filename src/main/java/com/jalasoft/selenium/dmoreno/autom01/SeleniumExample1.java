2b52fd0699fac3a80a22ebefa767292fe9be1618
Daniel Moreno <daniel.moreno@jalasoft.com>
2017-03-13 19:43:02 -0400

    Added selenium examples.

--- src/main/java/com/jalasoft/selenium/dmoreno/autom01/SeleniumExample1.java
+++ src/main/java/com/jalasoft/selenium/dmoreno/autom01/SeleniumExample1.java
@@ -5,17 +5,53 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Daniel Moreno on 3/10/2017.
  */
 public class SeleniumExample1 {
 
+    /**
+     *
+     */
+    @Test
+    public void logicTest() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        final long implicitWaitTimeout = 20;
+        driver.manage().timeouts().implicitlyWait(implicitWaitTimeout, TimeUnit.SECONDS);
+        final long driverWaitTimeout = 30;
+        WebDriverWait driverWait = new WebDriverWait(driver, driverWaitTimeout);
+
+        driver.get("https://login.salesforce.com/");
+
+        WebElement userInput = driver.findElement(By.id("username"));
+        WebElement passwordInput = driver.findElement(By.id("password"));
+        WebElement logInButton = driver.findElement(By.id("Login"));
+
+        userInput.sendKeys("daniel.moreno@jalasoft.com");
+        passwordInput.sendKeys("Curso2017");
+        logInButton.click();
+
+        WebElement appLauncherButton = driverWait.until(
+                ExpectedConditions.presenceOfElementLocated(By.cssSelector("div.bBottom button.bare")));
+        appLauncherButton.click();
+    }
+
+    /**
+     * @throws InterruptedException exception
+     */
     @Test
     public void testSelenium1() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        final long driverWaitTimeout = 30;
+        WebDriverWait driverWait = new WebDriverWait(driver, driverWaitTimeout);
 
         driver.get("http://only-testing-blog.blogspot.in/2013/09/test.html");
 
@@ -28,10 +64,9 @@ public class SeleniumExample1 {
         System.out.println(lastNameTestField.isEnabled());
 
         WebElement bikeCheckbox = driver.findElement(By.cssSelector("input[value='Bike']"));
-        if(!bikeCheckbox.isSelected()) {
+        if (!bikeCheckbox.isSelected()) {
             bikeCheckbox.click();
-        }
-        else if(!bikeCheckbox.isSelected()) {
+        } else if (!bikeCheckbox.isSelected()) {
             bikeCheckbox.click();
         }
 
@@ -40,10 +75,12 @@ public class SeleniumExample1 {
 
         Select selectBox = new Select(driver.findElement(By.xpath("//select")));
 
+        final long waitInterval = 1500;
         selectBox.selectByVisibleText("Audi");
-        Thread.sleep(1500);
+        Thread.sleep(waitInterval);
+
         selectBox.selectByValue("UK");
-        Thread.sleep(1500);
+        Thread.sleep(waitInterval);
         selectBox.selectByIndex(2);
 
         Select fromSelectBox = new Select(driver.findElement(By.name("FromLB")));
@@ -55,5 +92,10 @@ public class SeleniumExample1 {
         for (WebElement option : toSelectBox.getOptions()) {
             System.out.println(option.getText());
         }
+
+        WebElement submitButton = driver.findElement(By.id("submitButton"));
+        driverWait.until(ExpectedConditions.elementToBeClickable(submitButton));
+
+        System.out.println("submitButton is clickable.");
     }
 }
